9dcc14466fea41fc3195b197fc8865f1f527eaae
Aziz <azizbibitov11@gmail.com>
2025-07-19 10:41:26 +0500

    Initial Commit

---
+++ StocksSnapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksSnapKit
+//
+//  Created by Aziz Bibitov on 19/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
